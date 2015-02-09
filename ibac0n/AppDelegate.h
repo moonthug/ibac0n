e488f8a0f6c912a9aec3073da9aa1c3958d5ebb8
Alex Coulcher <alex.coulcher@ogilvy.com>
2015-02-09 14:59:19 +0000

    Tweaks

--- ibac0n/AppDelegate.h
+++ ibac0n/AppDelegate.h
@@ -10,10 +10,10 @@
 
 @interface AppDelegate : NSObject
 
-@property NSString * beacon_uuid;
-@property uint16_t beacon_minor_v;
-@property uint16_t beacon_major_v;
-@property int8_t beacon_power;
+@property (assign,nonatomic) NSString * beacon_uuid;
+@property (assign,nonatomic) uint16_t beacon_minor_v;
+@property (assign,nonatomic) uint16_t beacon_major_v;
+@property (assign,nonatomic) int8_t beacon_power;
 
 - (void) setupWithUUID:(NSString *)uuid
          major:(uint16_t)major
